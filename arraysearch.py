869311f0115a50e3b163185ddd68dbf70a608b73
manogna-45 <manognapoloji@gmail.com>
2026-07-24 21:37:55 +0530

    Create arraysearch.py

---
+++ arraysearch.py
@@ -0,0 +1,8 @@
+class Solution:
+    def search(self, arr, x):
+        # code here
+        n = len(arr)
+        for i in range(0,n):
+            if arr[i] == x:
+                return i
+        return -1
