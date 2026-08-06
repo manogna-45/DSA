421496a7669e07b253ace0db0ee3c4ecd9969025
manogna-45 <manognapoloji@gmail.com>
2026-08-06 21:14:45 +0530

    Create selectionsort.py

---
+++ selectionsort.py
@@ -0,0 +1,11 @@
+class Solution: 
+    def selectionSort(self, arr):
+        # code here
+        n = len(arr)
+        for i in range(n):
+            min_ind = i
+            for j in range(i+1,n):
+                if arr[j] < arr[min_ind]:
+                    min_ind = j
+            arr[i], arr[min_ind] = arr[min_ind], arr[i]
+        return arr
