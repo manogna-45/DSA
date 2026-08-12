2a297c36cd6156fea89cb6da64c185efa0f2f447
manogna-45 <manognapoloji@gmail.com>
2026-08-12 21:06:31 +0530

    Create romantointeger.py

---
+++ romantointeger.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        result = 0
+        n = len(s)
+        for i in range(n):
+            if i < n - 1 and translations[s[i]] < translations[s[i + 1]]:
+                result -= translations[s[i]]
+            else:
+                result += translations[s[i]]
+        return result
